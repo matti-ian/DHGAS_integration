bb6f0bca059ba8c3e7b095d12b1548af61bceea3
matti-ian <mattiian43@gmail.com>
2024-06-23 23:46:25 +0300

    Setup and requirements
    Created a setup file to install dependencies and create a virtual environment
    
    Updated the list of requirements.

---
+++ setup.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM create a virtual environment
+python -m venv integration
+
+REM install prerequisites
+pip install -r requirements.txt
+
+git clone https://github.com/wondergo2017/DHGAS
+pip install -e DHGAS
+
+git clone https://github.com/charleshsc/CommFormer
+cd CommFormer
+pip install -r requierments.txt
+
+REM python -m site --user-site  user_python_path.txt
+cd integration\Lib\site-packages
+echo ..\DHGAS\dhgas > dhgas.pth
+echo ..\CommFormer\commfommer > CommFormer.pth
