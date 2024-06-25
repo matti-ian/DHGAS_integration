dd6053901129a17dc611e94716a2407f1d75b7be
matti-ian <mattiian43@gmail.com>
2024-06-25 12:22:40 +0300

    Update setup.bat and readme

--- setup.bat
+++ setup.bat
@@ -11,30 +11,20 @@ pip install -e DHGAS
 
 git clone https://github.com/charleshsc/CommFormer
 cd CommFormer
-pip install -r requierments.txt
+pip install -r requirments.txt
 
-REM python -m site --user-site  user_python_path.txt
-setlocal
 
-REM Specify the directory and file name
-set "directory=CommFormer"
-set "file_name=CommFormer.pth"
+REM Get the absolute path of the current directory
+for %%I in (.) do set "absolute_path=%%~fI"
 
-REM Check if the directory exists
-if not exist "%directory%" (
-    echo Directory does not exist: %directory%
-    exit /b 1
-)
+REM Create a file and write the absolute path into it
 
-REM Get the absolute path of the directory
-for %%I in ("%directory%") do set "absolute_path=%%~fI"
+echo %absolute_path%\CommFormer.pth > CommFormer.pth
 
-REM Create the file and write the absolute path into it
-set "file_path=%directory%\%file_name%"
-echo %absolute_path% > "%file_path%"
+cd ..
 
 REM copy data folder
-robocopy "DHGAS_integration\DHGAS data\ecomm" "DHGAS_integration\DHGAS\data\ecomm"
+robocopy "DHGAS data\ecomm" "DHGAS\data\ecomm"
 
 REM activate virtual environment
 integration/Scripts/activate
